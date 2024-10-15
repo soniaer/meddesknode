7476ac346f1185e862833749c39e95e854e51185
soniaer <soniaer987@gmail.com>
2024-10-15 11:41:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors"); 
 const sql = require('mssql');
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '500mb'}));
 const port = process.env.PORT || 3004;
 const server = app.listen(port, () =>
 console.log(`mqttengine app listening on port ${port}!`)
